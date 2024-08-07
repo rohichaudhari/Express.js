c8acd2bb2bbf681d185b482170bfdba7c2808a00
Rohini <rohinichaudhari0307@gmail.com>
2024-08-07 20:55:56 +0530

    7/8/2024

--- lecture1.js
+++ lecture1.js
@@ -1,4 +1,5 @@
 var express = require('express')
+const server=express();
 // var app = express()
 
 // app.get('/', function (req, res) {
@@ -9,33 +10,60 @@ var express = require('express')
 //     console.log("server start at http://localhost:3000");
 // })
 
-const server=express();
-server.get("/",(req,res)=>{
-  res.write('welcome to Express server');
-  res.end();
-})
+// Day-3
+// const fs=require('fs');
+// const data=fs.readFileSync('./friend.json','utf-8')
 
-server.get("/user",(req,res)=>{
+// server.get("/friend",(req,res)=>{
+//   res.status(200);
+//   res.json(JSON.parse(data));
+// })
+
+
+// const server=express();
+// server.get("/",(req,res)=>{
+//   res.write('welcome to Express server');
+//   res.end();
+// })
+
+// middleware
+// let middleware=(req,res,next)=>{
+//   console.log(req.query);
+// }
+// server.use(middleware);
+
+let middleware=(req,res,next)=>{
+  // console.log(req.query);
+  if(req.query.password==='1234'){
+    console.log('sucess');
+    next();
+  }
+  else{
+    return res.json({message:'incorect password'})
+  }
+}
+
+server.get("/user",middleware,(req,res)=>{
   res.status(200);
   res.json({message:'use GET method'});
 })
-
-server.post("/user",(req,res)=>{
-  res.status(201);
-  res.json({message:'use GET method'});
-})
-server.put("/user",(req,res)=>{
-  res.status(200);
-  res.json({message:'use put method'});
-})
-server.patch("/admin",(req,res)=>{
-  res.status(200);
-  res.json({message:'use patch method'});
-})
-server.delete("/admin",(req,res)=>{
-  res.status(200);
-  res.json({message:'use delete method'});
-})
-server.listen(9876,()=>{
-  console.log(`server start at http://localhost:9876`);
+// Day 3 end
+// server.post("/user",(req,res)=>{
+//   res.status(201);
+//   res.json({message:'use GET method'});
+// })
+// server.put("/user",(req,res)=>{
+//   res.status(200);
+//   res.json({message:'use put method'});
+// })
+// server.patch("/admin",(req,res)=>{
+//   res.status(200);
+//   res.json({message:'use patch method'});
+// })
+// server.delete("/admin",(req,res)=>{
+//   res.status(200);
+//   res.json({message:'use delete method'});
+// })
+server.listen(1034,()=>{
+  console.log(`server start at http://localhost:1034`);
 })
