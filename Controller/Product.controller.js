33e6c47849ad51fa060bf45a0d365049ea92e083
Rohini <rohinichaudhari0307@gmail.com>
2024-08-14 11:35:05 +0530

    14/8/2024

--- Controller/Product.controller.js
+++ Controller/Product.controller.js
@@ -42,17 +42,77 @@
 //         res.json({products,message:"delete successfully"});
 // }
 
-const Product=require('../Model/product.model');
-exports.addNewproduct= async (req,res)=>{
+
+// const Product=require('../Model/product.model');
+// exports.addNewproduct= async (req,res)=>{
+//     try{
+//         console.log(req.body);
+//         const product= await Product.create({...req.body});
+//         // Product.save();
+//         res.status(201).json({product,message:"user added"});
+//     }
+//     catch(err){
+//         console.log(err);
+//         res.status(500).json({message:"Internal server error"});
+
+//     }
+// };
+
+
+const product=require("../Model/product.model");
+// add new Product
+exports.addNewProduct=async(req,res)=>
+    {
     try{
         console.log(req.body);
-        const product= await Product.create({...req.body});
-        // Product.save();
-        res.status(201).json({product,message:"user added"});
+        const {firstname ,lastname,email,age,hobbies,address}=req.body;
+        let Product =await product.findOne({email:email});
+        if(Product)
+        {
+            return res.status(400).json({message:"product already exists......"});
+        }
+            
+            Product=await product.create({
+            firstname ,lastname,email,age,hobbies,address,
+            });
+            // Product.save();
+            res.status(201).json({message:"product Added"});
     }
     catch(err){
         console.log(err);
-        res.status(500).json({message:"Internal server error"});
-
+        res.status(500).json({message:"Internal server error"})
     }
 };
+
+// Get all product
+exports.getAllProduct=async(req,res)=>
+{
+    try{
+        let Products=await product.find();
+        res.status(200).json(Products);
+    }
+    catch(error)
+    {
+        console.log(error)
+        res.status(500).json({message:"Internal server error"});
+    }
+};
+
+//get user
+exports.getProduct=async (req,res)=>{
+    try{
+        let Product= await product.findOne({firstname:req.query.firstname});
+        if(!Product){
+            return res.status(404).json({message:"Product not Found.."})
+        }
+        res.status(200).json(Product);
+    }
+        catch(error)
+        {
+            console.log(error);
+            res.status(500).json({message:"Internal server error"});
+        }
+        
+    }
+        
+    
--- Routers/Product.routes.js
+++ Routers/Product.routes.js
@@ -2,30 +2,15 @@ const express = require('express')
 const productRoutes = express.Router();
 
 const {
-    addNewproduct
-//   getAllProduct,
-//     getProduct,
-//     replaceProduct,
-//     updateProduct,
-//     deleteProduct
+    addNewProduct,
+    getAllProduct,
+    getProduct
+
 }=require("../Controller/Product.controller.js");
 
-// Add new product =>create
-productRoutes.post("/",addNewproduct);
 
-// Get all product=> read
-// productRoutes.get("/", getAllProduct);
-
-// get single product =>read
-// productRoutes.get("/:id",  getProduct);
-
-// replace data =>put
-// productRoutes.put("/:id", replaceProduct);
-
-// update data=>patch
-// productRoutes.patch("/:id", updateProduct);
-
-// delete data=>delete
-// productRoutes.patch("/:id", deleteProduct);
+productRoutes.post("/",addNewProduct);
+productRoutes.get("/",getAllProduct);
+productRoutes.get("/get-product",getProduct);
 
 module.exports=productRoutes;
